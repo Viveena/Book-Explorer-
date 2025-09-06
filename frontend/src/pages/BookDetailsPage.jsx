88b12722b6b815cf5922a2f8fb518bc0d6a13743
Viveena Khatri <157346644+Viveena@users.noreply.github.com>
2025-09-06 10:11:45 +0530

    Update BookDetailsPage.jsx

--- frontend/src/pages/BookDetailsPage.jsx
+++ frontend/src/pages/BookDetailsPage.jsx
@@ -39,7 +39,7 @@ const BookDetailsPage = () => {
             setLoading(true);
             setError(null);
             try {
-                const response = await axios.get(`http://localhost:5000/api/books/${id}`);
+                const response = await axios.get(`https://book-explorer-backend-c83o.onrender.com/api/books/${id}`);
                 setBook(response.data);
             } catch (err) {
                 setError('Failed to fetch book details.');
